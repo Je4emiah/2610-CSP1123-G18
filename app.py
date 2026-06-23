ee74d105db3df56c3231453874be9b95c378e142
Jeremiah Chua Yong Ern <jeremiahchuayongern@Samuels-MacBook-Air.local>
2026-06-23 12:36:28 +0800

    Fix dependencies

--- app.py
+++ app.py
@@ -924,7 +924,8 @@ def init_db():
     
     print("Database refreshed and ready with Full Name schema parameters & Security Questions!")
 
+with app.app_context():
+    init_db()
+
 if __name__ == '__main__':
-    with app.app_context():
-        init_db()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
